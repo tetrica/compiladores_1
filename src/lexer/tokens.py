ebbae23329cd35e88a3f7dc057986bc35c6a92e6
fabio luiz <fabio@paikka.me>
2019-03-16 16:30:02 -0400

    Sobrescreve metodo __str__ da tupla de token

--- src/lexer/tokens.py
+++ src/lexer/tokens.py
@@ -7,6 +7,12 @@ __token = namedtuple('Token', [
     'line'
 ])
 
+def to_string(obj):
+    return f"T<'{obj.content}', {obj.token_type}>"
+
+
+__token.__str__ = to_string
+
 class Token_type(Enum):
     ID = auto()
     INVALID = auto()
@@ -18,8 +24,9 @@ def new_token(content, line):
     return __token(
         content=content,
         line=line,
-        token_type=__match_type(content)
+        token_type=None
         )
 
+#FIXME(Fabio): precisamos implementar isso
 def __match_type(string):
     return Token_type.ID
