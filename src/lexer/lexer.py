e62f963699f56e0cfadf4273d962e3ff8b427417
fabio luiz <fabio@paikka.me>
2019-03-16 16:29:30 -0400

    Implementa parsing lexico

--- src/lexer/lexer.py
+++ src/lexer/lexer.py
@@ -1,9 +1,40 @@
 from . import token_list as TL
+import unicodedata as ud
+
+NO_SEMANTICS = (' ', '\n', '\t')
+SYMBOLS = (',', ';', ':', '+', ':=')
+KEYWORDS = ('var', 'integer', 'real', 'if', 'then')
 
 def tokenize(path):
+    buff = ''
+    possible_double_symbol = False
     token_list = TL.Token_list()
 
     with open(path) as file:
-        for i, line in enumerate(file):
+        for index, line in enumerate(file):
             for char in line:
-                print("ate amanha")
+                if possible_double_symbol:
+                    possible_double_symbol = False
+
+                    if char == '=':
+                        token_list.add_new_token(':=', index)
+                    else:
+                        token_list.add_new_token(':', index)
+                    
+                    continue
+
+                if not char.isalnum():
+                    if buff:
+                        token_list.add_new_token(buff, index)
+                        buff = ''
+
+                    if char == ':':
+                        possible_double_symbol = True
+                    elif char in SYMBOLS:
+                        token_list.add_new_token(char, index)
+
+                elif char not in NO_SEMANTICS:
+                    buff += char
+            else:
+                buff = ""
+    return token_list
