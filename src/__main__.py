e7fbdc0c4ca8c185ae237c4a5cb8c79b9df31d0c
fabio luiz <fabio@paikka.me>
2019-03-16 16:30:57 -0400

    atualiza chamada do tokenize

--- src/__main__.py
+++ src/__main__.py
@@ -4,4 +4,7 @@ import sys
 if __name__ == '__main__':
     file_code_path = sys.argv[1]
     
-    tokenize(file_code_path)
+    token_list = tokenize(file_code_path)
+
+    for token in token_list:
+        print(token)
