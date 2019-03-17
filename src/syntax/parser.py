f7abd1bdc357e4253f67a8d6835c16a6d03828ef
fabio luiz <fabio@paikka.me>
2019-03-17 15:52:33 -0400

    define assinatura de funções de regras semanticas

---
+++ src/syntax/parser.py
@@ -0,0 +1,36 @@
+def parse(token_list):
+    Z(token_list)
+
+def D(token_list):
+    pass
+
+def E(token_list):
+    pass
+
+def I(token_list):
+    pass
+
+def K(token_list):
+    pass
+
+def L(token_list):
+    pass
+
+def O(token_list):
+    pass
+
+def R(token_list):
+    pass
+
+def S(token_list):
+    pass
+
+def T(token_list):
+    pass
+
+def X(token_list):
+    pass
+
+def Z(token_list):
+    I(token_list)
+    S(token_list)
